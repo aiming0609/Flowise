12fad539976d1842516de1f21a92789be1857161
aiming0609 <aiming0609@aliyun.com>
2025-01-14 10:23:48 +0800

    将测试环境的端口号改为9090

--- packages/ui/vite.config.js
+++ packages/ui/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 import dotenv from 'dotenv'
+import { resolve } from 'path'
+import { defineConfig } from 'vite'
 
 export default defineConfig(async ({ mode }) => {
     let proxy = undefined
@@ -37,7 +37,7 @@ export default defineConfig(async ({ mode }) => {
         server: {
             open: true,
             proxy,
-            port: process.env.VITE_PORT ?? 8080,
+            port: process.env.VITE_PORT ?? 9090,
             host: process.env.VITE_HOST
         }
     }
